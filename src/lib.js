02216177595b08b6666e9aa88cdd576518fe8bf1
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-11-22 10:01:26 +0530

    Modify reduce and introduce introduce

--- src/lib.js
+++ src/lib.js
@@ -6,21 +6,39 @@ const map = function(mapper, source) {
   return mappedArray;
 }
 
-const filter = function(predictor, source) {
+const filter = function(predicate, source) {
   let filterArray = new Array();
   for(element of source) {
-    if(predictor(element)) {
+    if(predicate(element)) {
       filterArray.push(element);
     }
   }
   return filterArray;
 }
 
-const reduce = function(reducer, init, source) {
-  for(element of source) {
+const reduce = function(reducer, source, init) {
+  let sourceCopy = source.slice();
+  if(init == undefined) {
+    init = sourceCopy.shift();
+  }
+  for(element of sourceCopy) {
     init = reducer(init, element);
   }
   return init;
 }
 
-module.exports = { map, filter, reduce };
+const reduceRecursive = function(reducer, source, init) {
+  let sourceCopy = source.slice();
+  if(init == undefined) {
+    init = sourceCopy.shift();
+  }
+  if(sourceCopy.length == 0) {
+    return init;
+  }
+  if(sourceCopy.length == 1) {
+    return reducer(init, sourceCopy.pop());
+  } 
+  return reducer( sourceCopy.pop(), reduceRecursive(reducer, sourceCopy, init ) );
+}
+
+module.exports = { map, filter, reduce, reduceRecursive};
